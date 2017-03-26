55a0188498cddc2a089c9634c2bb0b15dbb2eb67
sslampa <sslampa@gmail.com>
2017-03-26 11:14:16 -0700

    Add basic panels with descriptions

---
+++ model.js
@@ -0,0 +1,24 @@
+var skills = {
+  "Ruby": ["Ruby on Rails", "Sinatra", "RSpec", "Capybara", "Filterrific", "Kaminari", "Gmaps4Rails"],
+  "Python": ["pandas", "matplotlib", "Seaborn", "Jupyter", "requests"],
+  "HTML, CSS, JavaScript": ["jQuery", "AJAX", "Bootstrap", "HAML", "SCSS"],
+  "SQL": ["PostgreSQL", "MySQL"],
+  "Environments": ["Windows", "Linux (Ubuntu/Fedora)", "Mac OS X"]
+};
+
+var projects = {
+  "VolunteerUP": ["Cool"],
+  "Battleship": ["Even cooler"],
+  "To-Do List": ["Stuff"],
+  "Twitch.tv": ["this works"]
+};
+
+var experience = {
+  "Xander": ["Here it is!"],
+  "UC Davis - Physics Department": ["Stuff to see"]
+};
+
+var volunteer = {
+  "RailsBridge": ["this is here"],
+  "UC Davis - Mathematics and Science Teaching": ["stuff here"]
+}
--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+$(document).ready(function() {
+  addPanel(skills, "#skill-section");
+  addPanel(projects, "#project-section");
+  addPanel(experience, "#experience-section");
+  addPanel(volunteer, "#volunteer-section");
+});
+
 function showTab(tab) {
   $(".content-section").hide();
-  // $("#"+tab).show();
   $("#"+tab).fadeIn("slow");
+}
+
+function addPanel(panel, id) {
+  for (key in panel) {
+    var list = "";
+    for (var i=0, len=panel[key].length; i < len; i++) {
+      list += '<li>' + panel[key][i] + '</li>';
+    }
+
+    $(id).append(
+      '<div class="col-md-3"><div class="panel panel-default">' +
+      '<div class="panel-heading">' + key + '</div>' +
+      '<div class="panel-body"><ul>' + list + '</ul></div>' +
+      '</div></div>'
+    )
+  }
 }
