d807b900306627981007189619a0474963b5fd31
sslampa <sslampa@gmail.com>
2017-03-26 21:24:43 -0700

    Add content to eaach field

--- model.js
+++ model.js
@@ -1,24 +1,87 @@
 var skills = {
-  "Ruby": ["Ruby on Rails", "Sinatra", "RSpec", "Capybara", "Filterrific", "Kaminari", "Gmaps4Rails"],
-  "Python": ["pandas", "matplotlib", "Seaborn", "Jupyter", "requests"],
-  "HTML, CSS, JavaScript": ["jQuery", "AJAX", "Bootstrap", "HAML", "SCSS"],
-  "SQL": ["PostgreSQL", "MySQL"],
-  "Environments": ["Windows", "Linux (Ubuntu/Fedora)", "Mac OS X"]
+  "Ruby": {
+    "content": ["Ruby on Rails", "Sinatra", "RSpec", "Capybara", "Filterrific", "Kaminari", "Gmaps4Rails"]
+  },
+  "Python": {
+    "content": ["pandas", "matplotlib", "Seaborn", "Jupyter", "requests"]
+  },
+  "HTML, CSS, JavaScript": {
+    "content": ["jQuery", "AJAX", "Bootstrap", "HAML", "SCSS"]
+  },
+  "SQL": {
+    "content": ["PostgreSQL", "MySQL"]
+  },
+  "Environments": {
+    "content": ["Windows", "Linux (Ubuntu/Fedora)", "Mac OS X"]
+  }
 };
 
 var projects = {
-  "VolunteerUP": ["Cool"],
-  "Battleship": ["Even cooler"],
-  "To-Do List": ["Stuff"],
-  "Twitch.tv": ["this works"]
+  "VolunteerUP": {
+    "link": "https://github.com/stephaniejue/VolunteerUP",
+    "content": [
+      "Rails-based multi-page website that matches volunteer candidates with organizations in need of assistance",
+      "Linked Filterrific, Kaminari, and Gmaps4Rails gems to sync the information in the filtered table and map",
+      "Setup the schema to create a many-to-many relationship for users, events, and organizations using junction tables",
+      "Designed a recommendation algorithm to recommedn the top 3 events a user could join",
+      "Tested the data models and UI using RSpec and Capybara to ensure any change to the code did not affect other areas"
+    ]
+  },
+  "Battleship": {
+    "link": "https://github.com/sslampa/battleship",
+    "content": [
+      "Single-player Battleship game",
+      "Generated game layout using HTML and CSS with flexbox",
+      "jQuery used for game interaction and JavaScript for game mechanics"
+    ]
+  },
+  "To-Do List": {
+    "link": "https://github.com/sslampa/to-do-list",
+    "content": [
+      "To-Do List App that adds, removes, and updates items",
+      "Uses JavaScript to create a mock model",
+      "Uses jQuery to create the controller that creates, updates, and deletes items"
+    ]
+  },
+  "Twitch.tv": {
+    "link": "https://github.com/sslampa/twitch-repo",
+    "content": [
+      "Gather and analyze data from Twitch.tv",
+      "Retrieved data with Twitch.tv API and Python requests library to periodically add data to MySQL database",
+      "Reported findings using the Python libraries, Seaborn and matplotlib, for visualization and Jupyter",
+      "Explored and analyzed data using pandas library to better understand streaming habits developed by individuals and team streamers"
+    ]
+  }
 };
 
 var experience = {
-  "Xander": ["Here it is!"],
-  "UC Davis - Physics Department": ["Stuff to see"]
+  "Xander": {
+    "content": [
+      "Deliberated with team over UI design to find most efficient layout for a user to accept/reject a sample",
+      "Implemented the modular design using uni.js, jQuery, and Bootstrap to make reusable components",
+      "Interfaced with the COO to ensure all requirements were being met"
+    ]
+  },
+  "UC Davis - Physics Department": {
+    "content": [
+      "Aided in cleaning and analyzing data derived from the Sloan Digital Sky Survey",
+      "Extensive use of Interactive Data Language (IDL) to visualize and model data",
+      "Lead collaboration with head researcher to ensure conclusions contained minimal errors to prevent future problems"
+    ]
+  }
 };
 
 var volunteer = {
-  "RailsBridge": ["this is here"],
-  "UC Davis - Mathematics and Science Teaching": ["stuff here"]
+  "RailsBridge": {
+    "content": [
+      "Helped create website using Sinatra, HAML, and SCSS",
+      "Served as an organizer and TA for the RailsBridge class"
+    ]
+  },
+  "UC Davis - Mathematics and Science Teaching": {
+    "content": [
+      "Collaborated with lead teaching assistant to encourage group discussion in the kinematics and mechanics of Physics subject material and general science",
+      "Interfaced with professors and elementary teachers, consulting on how to better communicate Physics subject material to college peers and elementary demographic"
+    ]
+  }
 }
--- script.js
+++ script.js
@@ -13,13 +13,23 @@ function showTab(tab) {
 function addPanel(panel, id) {
   for (key in panel) {
     var list = "";
-    for (var i=0, len=panel[key].length; i < len; i++) {
-      list += '<li>' + panel[key][i] + '</li>';
+    var url = "";
+
+    for (var content in panel[key]) {
+      if (content === 'link') {
+        url = '<a target="_blank" href="' + panel[key][content] + '">' + key + '</a>';
+      } else {
+        for (var i=0, len=panel[key][content].length;i < len; i++) {
+          list += '<li>' + panel[key][content][i] + '</li>';
+        }
+      }
     }
 
+    console.log(url)
+
     $(id).append(
-      '<div class="col-md-3"><div class="panel panel-default">' +
-      '<div class="panel-heading">' + key + '</div>' +
+      '<div class="col-md-4"><div class="panel panel-default">' +
+      '<div class="panel-heading">' + (url || key) + '</div>' +
       '<div class="panel-body"><ul>' + list + '</ul></div>' +
       '</div></div>'
     )
